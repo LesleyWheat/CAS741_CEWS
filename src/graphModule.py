1d080f9d13a128ba2ce8207d4814bffea4f5297a
LIVRE\Lesley <wheatd@mcmaster.ca>
2023-04-09 12:14:11 -0400

    Graph considers now takes very close cases into consideration

--- src/graphModule.py
+++ src/graphModule.py
@@ -1,20 +1,18 @@
 # File description
 # Constructs a relative neighbourhood graph over a dataset
-# ----------------------------------------------------------------
+#----------------------------------------------------------------
 # Import libraries
 import numpy as np
 import scipy.spatial
 from math import isclose
 
 # Local constants
-TOL_FLOAT = 1e-8
+TOL_FLOAT = 1e-10
 
-# ----------------------------------------------------------------
+#----------------------------------------------------------------
 # Global Functions
-
-
 def relativeNeighbourGraph(data):
-    # Constructs the relative neighbour graph for a given
+    # Constructs the relative neighbour graph for a given 
     # set of points
 
     # Calculate distance between all points
@@ -26,15 +24,13 @@ def relativeNeighbourGraph(data):
     for i in range(0, n-1):
         for j in range(i+1, n):
             edge = checkEdge_RNG(distanceMatrix, i, j)
-            V[i, j] = edge
-            V[j, i] = edge
+            V[i,j] = edge
+            V[j,i] = edge
 
-    return (V)
+    return(V)
 
-# ----------------------------------------------------------------
+#----------------------------------------------------------------
 # Local Functions
-
-
 def checkEdge_RNG(distanceMatrix, a, b):
     # Check if an edge exists between two points
     n = distanceMatrix.shape[0]
@@ -50,9 +46,12 @@ def checkEdge_RNG(distanceMatrix, a, b):
         E = True
         for i in range(0, n):
             # Only check points not on top of each other
-            if (distanceMatrix[a, i] > 0) & (distanceMatrix[b, i] > 0):
-                if max(distanceMatrix[a, i], distanceMatrix[b, i]) < distance_ab:
+            if (distanceMatrix[a,i] > 0) & (distanceMatrix[b,i] > 0):
+                max_dist = max(distanceMatrix[a,i], distanceMatrix[b,i])
+
+                if not isclose(max_dist, distance_ab, abs_tol=TOL_FLOAT) and max_dist < distance_ab:
+                    # if too close to call, then consider making edge
                     E = False
                     break
 
-    return (E)
+    return(E)
